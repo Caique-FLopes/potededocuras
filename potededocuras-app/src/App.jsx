7c0ce75a7183eaf56f1c17e44702651dacc159e2
Caique-FLopes <caiqueeflopes@hotmail.com>
2025-04-19 16:18:47 -0400

    inicio front

---
+++ potededocuras-app/src/App.jsx
@@ -0,0 +1,26 @@
+//import { useState } from 'react'
+
+function App() {
+  //const [count, setCount] = useState(0)
+  fetch('')
+  return (
+    <>
+      <main>
+        <section>
+          <a href="">
+            <img src="" alt="" srcset="" />
+          </a>
+        </section>
+        <section>
+          <form >
+            <select name="" id="">
+              
+            </select>
+          </form>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
